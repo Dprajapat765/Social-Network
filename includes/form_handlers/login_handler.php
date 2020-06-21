e3206abc1bd3fce19ecf06f76657da9b27085e73
Dinesh Prajapat <45159995+Dprajapat765@users.noreply.github.com>
2020-06-21 13:00:46 +0530

    updated login_handler

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -1,24 +1,30 @@
-<?php 
-
+<?php  
 if (isset($_POST['login_button'])) {
 	
-	$email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL); // sanitize email address
-
-	$_SESSION['log_email'] = $email; // store email in session variable
+	$email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL);
+	$_SESSION['log_email'] = $email; //store email in session variabel
 	$password = md5($_POST['log_password']); // get password
 
-	$check_database_query = mysqli_query($con, "SELECT * FROM  users WHERE email='$email' AND password='$password'");
+	$check_database_query = mysqli_query($con,"SELECT * FROM users WHERE email='$email' AND password='$password'");
 	$check_login_query = mysqli_num_rows($check_database_query);
 
 	if ($check_login_query==1) {
-		// you have successfully logged in
 		$row = mysqli_fetch_array($check_database_query);
-		$username =  $row['username'];
+		$username = $row['username'];
+
+		$user_closed_query = mysqli_query($con, "SELECT * FROM users WHERE user_closed='yes'"); 
+		if ($user_closed_query == 1) {
+			$reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE email='$email'");
+		}
 
 		$_SESSION['username'] = $username;
-		header("Location:index.php");
+		header("Location: index.php");
 		exit();
 	}
+	else{
+		array_push($error_array, "Username or Password is incorrect.<br>");
+	}
+}
 
 
 
