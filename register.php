0900f44e06d30f0cc41e84e8d05c9df447bc7bed
chandraprakashh <45662549+chandraprakashh@users.noreply.github.com>
2020-06-28 15:09:59 +0530

    Update register.php

--- register.php
+++ register.php
@@ -9,6 +9,8 @@ require 'includes/form_handlers/login_handler.php';
 <html>
 <head>
 	<title>SignUp or Login - Bhilwara Network</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 <!-- CSS only -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <!-- JS, Popper.js, and jQuery -->
@@ -17,155 +19,155 @@ require 'includes/form_handlers/login_handler.php';
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <!-- Add icon library -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Fontdiner+Swanky&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
 </head>
 <body>
-	<div class="forms">
-		<ul class="tab-group">
-			<li class="tab active"><a href="#reg_form">Sign Up</a></li>
-			<li class="tab"><a href="#login_form">Log In</a></li>
-		</ul>
-		
-		<form action="register.php" method="POST" id="login_form">
-		    <div class="input-field">
-				<input class="form-control" type="text" name="log_email" placeholder="Username" required=""><br>
-				<input class="form-control" type="password" name="log_password" placeholder="Password" required=""><br>
-				<input class="form-control btn btn-lg btn-success btn-block" type="submit" name="login_button" value="Login">
-				<br>
-				<?php if (in_array("<span style='color:#FF0000;'>Username or Password is incorrect.</span><br>", $error_array)) {
-					echo "<span style='color:#FF0000;'>Username or Password is incorrect.</span><br>";
-				}?>
-				<a href="#" id="forget_password">Forget Password?</a>
-			</div>
-		</form>
-		<form action="register.php" method="POST" id="reg_form">
-			<div class="input-field">
-		        <!-- ==============First Name ====================================== -->
-				<input class="form-control" type="text" name="reg_fname" placeholder="First Name" value="<?php  
-					if(isset($_SESSION['reg_fname'])){
-						echo $_SESSION['reg_fname'];
-					}
-				?>" required=""><br>
-				<!-- Display errors -->
-				<?php  
-					if (in_array("<span style='color:#FF0000;'>first name must be between 2-25 characters!!</span><br>", $error_array)) {
-						echo "<span style='color:#FF0000;'>first name must be between 2-25 characters!!</span><br>";
-					}
-				?>
-				<!--********************************************************************* -->
-				<!--********************************************************************* -->
-
-
-				<!-- ==============Last Name ====================================== -->
-				<input class="form-control" type="text" name="reg_lname" placeholder="Last Name"  value="<?php  
-					if(isset($_SESSION['reg_lname'])){
-						echo $_SESSION['reg_lname'];
-					}
-				?>" required=""><br>
-				<!-- Display errors -->
-				<?php  if (in_array("<span style='color:#FF0000;'>last name must be between 2-25 characters!!</span><br>", $error_array)) {
-					echo "<span style='color:#FF0000;'>last name must be between 2-25 characters!!</span><br>";
-				}?>
-				<!--********************************************************************* -->
-				<!--********************************************************************* -->
-
-
-				
-
-
-				<!-- ==============Username ====================================== -->
-				<input class="form-control" type="text" name="reg_username" placeholder="Username"  value="<?php  
-					if(isset($_SESSION['reg_username'])){
-						echo $_SESSION['reg_username'];
-					}
-				?>" required=""><br>
-				<!-- Display errors -->
-				<?php if (in_array("<span style='color:#FF0000;'>username can only contain a-z or numbers!!</span><br>", $error_array)) {
-						echo "<span style='color:#FF0000;'>username can only contain a-z or numbers!!</span><br>";}
-					else if (in_array("<span style='color:#FF0000;'>Username already exists!!</span><br>", $error_array)) {
-						echo "<span style='color:#FF0000;'>Username already exists!!</span><br>";}
-					else if (in_array("<span style='color:#FF0000;'>Username should be atleast of 6 characters!!</span><br>", $error_array)) {
-						echo "<span style='color:#FF0000;'>Username should be atleast of 6 characters!!</span><br>";}
-				?>
-				<!--********************************************************************* -->
-				<!--********************************************************************* -->
-
-
-				<!-- =========================Email ====================================== -->
-				<input class="form-control" type="email" name="reg_email" placeholder="Email Address" id="email"  value="<?php  
-					if(isset($_SESSION['reg_email'])){
-						echo $_SESSION['reg_email'];
-					}
-				?>"  required=""><br>
-
-				<!-- confirm email -->
-				<input class="form-control" type="email" name="reg_email2" placeholder="Confirm Email Address"   value="<?php  
-					if(isset($_SESSION['reg_email2'])){
-						echo $_SESSION['reg_email2'];
-					}
-				?>"  required=""><br>
-				<!-- Display errors -->
-				<?php if (in_array("<span style='color:#FF0000;'>Email already in use!!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>Email already in use!!</span><br>";}
-				else if (in_array("<span style='color:#FF0000;'>Invalid format!!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>Invalid format!!</span><br>";}
-				else if (in_array("<span style='color:#FF0000;'>Email doesn't match!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>Email doesn't match!</span><br>";}?>
-				<!--********************************************************************* -->
-				<!--********************************************************************* -->
-
-
-				<!-- Date of Birth input -->
-				<input class="form-control" type="date" name="dob" required=""><br>
-				<!--********************************************************************* -->
-
-
-				<!-- ==============Password ====================================== -->
-				<input class="form-control" type="password" name="reg_password" placeholder="Password" id="password"  required=""><br>
-
-				<!-- confirm password -->
-				<input class="form-control" type="password" name="reg_password2" placeholder="Confirm Password"  required=""><br>
-
-				<!-- Display errors -->
-				<?php if (in_array("<span style='color:#FF0000;'>password doesn't match!!</span><br>", $error_array)) {
-					echo "<span style='color:#FF0000;'>password doesn't match!!</span><br>";
-				}
-				else if (in_array("<span style='color:#FF0000;'>password can only contain a-z or numbers!!</span><br>", $error_array)) {
-					echo "<span style='color:#FF0000;'>password can only contain a-z or numbers!!</span><br>";
-				}
-				else if (in_array("<span style='color:#FF0000;'>password should atleast 6 character long or between 6-20!!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>password should atleast 6 character long or between 6-20!!</span><br>";
-				}?>
-				<!--********************************************************************* -->
-				<!--********************************************************************* -->
-
-				<input class="form-control btn btn-lg btn-info btn-block" type="submit" id="reg_form" name="register_button" value="Signup">
-		    </div>
-		</form>
-
-
-		
-	</div>
-
-
-
-	<div class="content">
-		<div class="logo">
+	<div class="logo">
 			<img src="assets/images/logo.png" alt="bhilwara network logo" width="80" height="80">
 			<span>Bhilwara Network</span>
 		</div>
-		<!-- slider -->
-		<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img class="d-block w-100" src="assets/images/slider_images/011.jpg" alt="First slide">
-				</div>
-				<div class="carousel-item">
-					<img class="d-block w-100" src="assets/images/slider_images/012.jpg" alt="Second slide">
-				</div>
-				<div class="carousel-item">
-					<img class="d-block w-100" src="assets/images/slider_images/013.jpg" alt="Third slide">
+<div class="container-fluid">
+	<div class="row">
+		<div class="content col-sm-8">
+			
+			<!-- slider -->
+			<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img style="height: 535px;" class="d-block w-100" src="assets/images/slider_images/011.jpg" alt="First slide">
+					</div>
+					<div class="carousel-item">
+						<img style="height: 535px;" class="d-block w-100" src="assets/images/slider_images/012.jpg" alt="Second slide">
+					</div>
+					<div class="carousel-item">
+						<img style="height: 535px;" class="d-block w-100" src="assets/images/slider_images/013.jpg" alt="Third slide">
+					</div>
 				</div>
 			</div>
 		</div>
+		<div class="forms col-sm-4">
+			<ul class="tab-group">
+				<li class="tab active"><a href="#reg_form">Sign Up</a></li>
+				<li class="tab"><a href="#login_form">Log In</a></li>
+			</ul>
+			
+			<form action="register.php" method="POST" id="login_form">
+			    <div class="input-field">
+					<input class="form-control" type="text" name="log_email" placeholder="Username" required=""><br>
+					<input class="form-control" type="password" name="log_password" placeholder="Password" required=""><br>
+					<input class="form-control btn btn-lg btn-success btn-block" type="submit" name="login_button" value="Login">
+					<br>
+					<?php if (in_array("<span style='color:#FF0000;'>Username or Password is incorrect.</span><br>", $error_array)) {
+						echo "<span style='color:#FF0000;'>Username or Password is incorrect.</span><br>";
+					}?>
+					<a href="#" id="forget_password">Forget Password?</a>
+				</div>
+			</form>
+			<form action="register.php" method="POST" id="reg_form">
+				<div class="input-field">
+			        <!-- ==============First Name ====================================== -->
+					<input class="form-control" type="text" name="reg_fname" placeholder="First Name" value="<?php  
+						if(isset($_SESSION['reg_fname'])){
+							echo $_SESSION['reg_fname'];
+						}
+					?>" required=""><br>
+					<!-- Display errors -->
+					<?php  
+						if (in_array("<span style='color:#FF0000;'>first name must be between 2-25 characters!!</span><br>", $error_array)) {
+							echo "<span style='color:#FF0000;'>first name must be between 2-25 characters!!</span><br>";
+						}
+					?>
+					<!--********************************************************************* -->
+					<!--********************************************************************* -->
+
+
+					<!-- ==============Last Name ====================================== -->
+					<input class="form-control" type="text" name="reg_lname" placeholder="Last Name"  value="<?php  
+						if(isset($_SESSION['reg_lname'])){
+							echo $_SESSION['reg_lname'];
+						}
+					?>" required=""><br>
+					<!-- Display errors -->
+					<?php  if (in_array("<span style='color:#FF0000;'>last name must be between 2-25 characters!!</span><br>", $error_array)) {
+						echo "<span style='color:#FF0000;'>last name must be between 2-25 characters!!</span><br>";
+					}?>
+					<!--********************************************************************* -->
+					<!--********************************************************************* -->
+
+
+					
+
+
+					<!-- ==============Username ====================================== -->
+					<input class="form-control" type="text" name="reg_username" placeholder="Username"  value="<?php  
+						if(isset($_SESSION['reg_username'])){
+							echo $_SESSION['reg_username'];
+						}
+					?>" required=""><br>
+					<!-- Display errors -->
+					<?php if (in_array("<span style='color:#FF0000;'>username can only contain a-z or numbers!!</span><br>", $error_array)) {
+							echo "<span style='color:#FF0000;'>username can only contain a-z or numbers!!</span><br>";}
+						else if (in_array("<span style='color:#FF0000;'>Username already exists!!</span><br>", $error_array)) {
+							echo "<span style='color:#FF0000;'>Username already exists!!</span><br>";}
+						else if (in_array("<span style='color:#FF0000;'>Username should be atleast of 6 characters!!</span><br>", $error_array)) {
+							echo "<span style='color:#FF0000;'>Username should be atleast of 6 characters!!</span><br>";}
+					?>
+					<!--********************************************************************* -->
+					<!--********************************************************************* -->
+
+
+					<!-- =========================Email ====================================== -->
+					<input class="form-control" type="email" name="reg_email" placeholder="Email Address" id="email"  value="<?php  
+						if(isset($_SESSION['reg_email'])){
+							echo $_SESSION['reg_email'];
+						}
+					?>"  required=""><br>
+
+					<!-- confirm email -->
+					<input class="form-control" type="email" name="reg_email2" placeholder="Confirm Email Address"   value="<?php  
+						if(isset($_SESSION['reg_email2'])){
+							echo $_SESSION['reg_email2'];
+						}
+					?>"  required=""><br>
+					<!-- Display errors -->
+					<?php if (in_array("<span style='color:#FF0000;'>Email already in use!!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>Email already in use!!</span><br>";}
+					else if (in_array("<span style='color:#FF0000;'>Invalid format!!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>Invalid format!!</span><br>";}
+					else if (in_array("<span style='color:#FF0000;'>Email doesn't match!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>Email doesn't match!</span><br>";}?>
+					<!--********************************************************************* -->
+					<!--********************************************************************* -->
+
+
+					<!-- Date of Birth input -->
+					<input class="form-control" type="date" name="dob" required=""><br>
+					<!--********************************************************************* -->
+
+
+					<!-- ==============Password ====================================== -->
+					<input class="form-control" type="password" name="reg_password" placeholder="Password" id="password"  required=""><br>
+
+					<!-- confirm password -->
+					<input class="form-control" type="password" name="reg_password2" placeholder="Confirm Password"  required=""><br>
+
+					<!-- Display errors -->
+					<?php if (in_array("<span style='color:#FF0000;'>password doesn't match!!</span><br>", $error_array)) {
+						echo "<span style='color:#FF0000;'>password doesn't match!!</span><br>";
+					}
+					else if (in_array("<span style='color:#FF0000;'>password can only contain a-z or numbers!!</span><br>", $error_array)) {
+						echo "<span style='color:#FF0000;'>password can only contain a-z or numbers!!</span><br>";
+					}
+					else if (in_array("<span style='color:#FF0000;'>password should atleast 6 character long or between 6-20!!</span><br>", $error_array)) {echo "<span style='color:#FF0000;'>password should atleast 6 character long or between 6-20!!</span><br>";
+					}?>
+					<!--********************************************************************* -->
+					<!--********************************************************************* -->
+
+					<input class="form-control btn btn-lg btn-info btn-block" type="submit" id="register_form" name="register_button" value="Signup">
+			    </div>
+			</form>		
+		</div>
 	</div>
+</div>
 
 
 	<div class="footer">
