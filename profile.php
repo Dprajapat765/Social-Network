5fd1d90d0577157086c825f3568c8b00eb28075c
Dinesh Prajapat <45159995+Dprajapat765@users.noreply.github.com>
2020-07-07 12:14:19 +0530

    profile files
    save htaccess file with .htaccess name

---
+++ profile.php
@@ -0,0 +1,20 @@
+<?php 
+include('includes/header.php');
+#session_destroy();
+
+?>
+
+
+
+			
+
+			<div class="main_column column">
+				This is a profile page
+
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
